00b150bfbbe116cbeb60f69a4489b56cdcc656a7
Nicolas MC <Ghost111nc@gmail.com>
2023-05-04 11:34:30 +1100

    Ajout route (html,md) pour affichage

--- app.py
+++ app.py
@@ -11,21 +11,41 @@ contenu_html = markdown.markdown(contenu_md)
 with open("index.html", "w") as fichier_html:
     fichier_html.write(contenu_html)
 
-#création du serveur local pour affichage du fichier html généré 
+#création du serveur local pour affichage du fichier html généré avec css et template
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
+
+#affichage du fichier markdown convertit en html
 @app.route('/')
 def index():
     
     with open('index.html', 'r') as f:
         content = f.read()
 
-    return render_template('template.html', 
+    return render_template('base.html', 
            content=content,
-           title="Test"
+           title="Projet Tutoré"
            )
+
+#affichage du fichier markdown de base
+@app.route('/md')
+def md():
+
+    with open('mon_fichier.md','r') as fichier :
+        content=fichier.read()
+    
+    return render_template('md.html',content=content)
+
+#affichage du fichier html de base
+@app.route('/html')
+def html():
+
+    with open('index.html','r') as fichier :
+        content=fichier.read()
+    
+    return render_template('html.html',content=content)
 
 if __name__ == '__main__':
     app.run()
