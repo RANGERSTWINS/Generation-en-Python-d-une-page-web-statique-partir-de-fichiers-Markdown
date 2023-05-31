98a9e77795bdf63304b6dba2a6a9600710c458fe
NicolasMarieCatherine <mariecatherinenicolas@gmail.com>
2023-05-31 16:41:56 +1100

    .

--- app.py
+++ app.py
@@ -2,32 +2,28 @@ import markdown
 from flask import Flask, render_template
 
 #convertir le fichier md en fichier html
-
-def generation_html(nom_du_fichier):
+def generation_html(chemin):
 
     print("Génération de la page Html ...")
 
     try :
-        fichier = "Md/"+nom_du_fichier
+        
+        with open(chemin, "r") as fichier_md:
+            contenu_md = fichier_md.read()
+
+        contenu_html = markdown.markdown(contenu_md)
+
+        with open("Output/index.html", "w") as fichier_html:
+            fichier_html.write(contenu_html)
+
+        print("Génération terminée !")
+
     except :
         print("Le fichier n existe pas . Recommencer")
 
-
-    with open(fichier, "r") as fichier_md:
-        contenu_md = fichier_md.read()
-
-    contenu_html = markdown.markdown(contenu_md)
-
-    with open("Output/index.html", "w") as fichier_html:
-        fichier_html.write(contenu_html)
-
-    print("Génération terminée !")
-
-
 nom_fichier = "mon_fichier"+".md"
 chemin = "Md/"+nom_fichier
-
-generation_html(nom_fichier)
+generation_html(chemin)
 
 #création du serveur local pour affichage du fichier html généré avec css et template
 
@@ -51,9 +47,9 @@ def index():
 @app.route('/md')
 def md():
 
-    global nom_fichier 
+    global chemin 
 
-    with open('Md/'+nom_fichier,'r') as fichier :
+    with open(chemin,'r') as fichier :
         content=fichier.read()
     
     return render_template('md.html',content=content)
