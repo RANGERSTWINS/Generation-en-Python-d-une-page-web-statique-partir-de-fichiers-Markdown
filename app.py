d15d7fabd897b53853e76c585b46219c27f39196
Nicolas MC <Ghost111nc@gmail.com>
2023-06-05 12:28:11 +1100

    .

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
-import markdown
 from flask import Flask, render_template
 
-#convertir le fichier md en fichier html
-def generation_html(chemin):
+def convert_md_to_html(md_file, html_file):
+    # Ouvrir le fichier Markdown en mode lecture
+    with open(md_file, 'r') as f:
+        contenu_md = f.read()
+    
+    # Convertir le contenu Markdown en HTML
+    html_content = ""
+    lines = contenu_md.split("\n")
 
-    print("Génération de la page Html ...")
-
-    try :
+    for line in lines:
+        # Gérer les balises spéciales Markdown
         
-        with open(chemin, "r") as fichier_md:
-            contenu_md = fichier_md.read()
+        # Titres
+        if line.startswith("#"):
+            level = line.count("#")
+            line = line.strip("#").strip()
+            html_content += f"<h{level}>{line}</h{level}>"
+        
+        # Paragraphes
+        elif line.strip() != "":
+            html_content += f"<p>{line}</p>"
 
-        contenu_html = markdown.markdown(contenu_md)
+        #Listes
+        if line.startswith("-"):
+            html_content += f"<ul> <li> {line} </li> </ul>"
 
-        with open("Output/index.html", "w") as fichier_html:
-            fichier_html.write(contenu_html)
+        #> blockquote 
+        if line.startswith(">"):
+            html_content += f"<blockquote>{line}</blockquote>"
 
-        print("Génération terminée !")
+    # Écrire le contenu HTML dans le fichier
+    with open(html_file, 'w') as f:
+        f.write(html_content)
 
-    except :
-        print("Le fichier n existe pas . Recommencer")
+# Exemple d'utilisation
+chemin = "Md/mon_fichier.md"
+sortie = "Output/index.html"
+convert_md_to_html(chemin, sortie)
 
-nom_fichier = "Exemple"+".md"
-chemin = "Md/"+nom_fichier
-generation_html(chemin)
-
-#création du serveur local pour affichage du fichier html généré avec css et template
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -34,8 +47,9 @@ app.static_folder = 'static'
 #affichage du fichier markdown convertit en html
 @app.route('/')
 def index():
+    global sortie
     
-    with open('Output/index.html', 'r') as fichier:
+    with open(sortie, 'r') as fichier:
         content = fichier.read()
 
     return render_template('base.html', 
@@ -57,8 +71,9 @@ def md():
 #affichage du fichier html de base
 @app.route('/html')
 def html():
+    global sortie
 
-    with open('Output/index.html','r') as fichier :
+    with open(sortie,'r') as fichier :
         content=fichier.read()
     
     return render_template('html.html',content=content)
